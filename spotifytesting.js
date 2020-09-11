d40645687864ab85f297f30a33e9f17ae4ab15ee
Grant Lu <grant.lu@princeton.edu>
2020-09-11 19:53:45 -0400

    fixed spotify search

--- spotifytesting.js
+++ spotifytesting.js
@@ -1,28 +1,43 @@
 const fetch = require("node-fetch");
+var btoa = require('btoa');
 
-var token = "BQAB6Z_fQguHqINSsIjjOL9CeYTyhp-9Sli8PikN9JnM-ztRCA2D1Otw_ZzsHPCNGc-0T63_nnWl0mS2HnaPUcWqo3szWM-06E2Y9eWIo6BcyILjP7EEAqbXyJM_bTfQN5uxM-7kppoqKnXNRA8T27-em6gCzHOYPh4CrlcRYFqGdacIWQ"
-// const _getGenres = async (token) => {
+const clientId = '78138a96f8be4efb8c281170c6350e39';
+const clientSecret = '742e4f609ede407baa123d5f777da2a6';
 
-//     const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
-//         method: 'GET',
-//         headers: { 'Authorization' : 'Bearer ' + token}
-//     });
+const _getToken = async () => {
 
-//     const data = await result.json();
-//     console.log(data.categories.items);
-// }
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+        },
+        body: 'grant_type=client_credentials'
+    });
+
+    const data = await result.json();
+    return data.access_token;
+}
 
 const _getPlaylistByGenre = async (token, genreId) => {
+    console.log(token)
 
     const limit = 10;
-    
+
     const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
         method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
+        headers: { 'Authorization': 'Bearer ' + token }
     });
 
     const data = await result.json();
     console.log(data.playlists.items);
 }
 
-_getPlaylistByGenre(token, "workout");
+async function main() {
+    let token = await _getToken();
+    await _getPlaylistByGenre(token, "workout");
+
+
+}
+
+main();

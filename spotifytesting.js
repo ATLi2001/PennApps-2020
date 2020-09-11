50c4413ddf82a9515822ec04c83e9ade85819af1
Grant Lu <grant.lu@princeton.edu>
2020-09-11 19:32:38 -0400

    spotify search

--- spotifytesting.js
+++ spotifytesting.js
@@ -1,10 +1,28 @@
-  /* Load the HTTP library */
-  var http = require("http");
+const fetch = require("node-fetch");
 
-  /* Create an HTTP server to handle responses */
+var token = "BQAB6Z_fQguHqINSsIjjOL9CeYTyhp-9Sli8PikN9JnM-ztRCA2D1Otw_ZzsHPCNGc-0T63_nnWl0mS2HnaPUcWqo3szWM-06E2Y9eWIo6BcyILjP7EEAqbXyJM_bTfQN5uxM-7kppoqKnXNRA8T27-em6gCzHOYPh4CrlcRYFqGdacIWQ"
+// const _getGenres = async (token) => {
 
-  http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }).listen(8888);
+//     const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+//         method: 'GET',
+//         headers: { 'Authorization' : 'Bearer ' + token}
+//     });
+
+//     const data = await result.json();
+//     console.log(data.categories.items);
+// }
+
+const _getPlaylistByGenre = async (token, genreId) => {
+
+    const limit = 10;
+    
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+    });
+
+    const data = await result.json();
+    console.log(data.playlists.items);
+}
+
+_getPlaylistByGenre(token, "workout");

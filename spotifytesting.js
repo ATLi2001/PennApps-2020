2ce3d5d17a31daac87447b932c8480f7a1f868a2
Grant Lu <grant.lu@princeton.edu>
2020-09-12 22:06:57 -0400

    updated logo

--- spotifytesting.js
+++ spotifytesting.js
@@ -3,6 +3,7 @@ var btoa = require('btoa');
 
 const clientId = '78138a96f8be4efb8c281170c6350e39';
 const clientSecret = '742e4f609ede407baa123d5f777da2a6';
+let res = []
 
 
 const _getToken = async () => {
@@ -49,8 +50,8 @@ async function getembbedURI(genreid) {
     let token = await _getToken();
     let genres = await _getGenres(token);
     let playlists = await _getPlaylistByGenre(token, genreid);
-    return(playlists[0].uri);
-    
+    return (playlists[0].uri);
+
 }
 
 async function main(genreid) {
@@ -58,7 +59,29 @@ async function main(genreid) {
     return playlisturi;
 }
 
+
+
 async function test() {
-    console.log(await main('workout'));
+    let arr = ['toplists', 'at_home', 'pop', 'mood', 'decades', 'hiphop', 'gaming', 'wellness', 'workout', 'chill', 'focus', 'sleep', 'party', 'metal', 'rock', 'edm_dance', 'dinner', 'jazz', 'rnb', 'romance']
+    arr.forEach(async val => {
+        try {
+            let URI = await main(val);
+            res.push({val, URI})
+            console.log(res)
+
+        } catch (error) {
+            console.log("this is error " + val)
+
+        }
+
+    })
+    await console.log(res)
+
+
 }
-test();
+async function finaltest() {
+    await test()
+    // await console.log(res);
+}
+
+finaltest();
